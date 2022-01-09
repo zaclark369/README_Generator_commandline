f2079f5285d7c15b084c8cb33cd3713a89f04f92
Zachary Clark <zacclark369@gmail.com>
2022-01-09 11:08:08 -0800

    readme generated, images folder and png and mp4 files added. fixed init function. changed writetofile, to writeFileSync

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# README_Generator_commandline
+# Readme Generator
+  ![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)
+  ## Description:
+  ___
+  A node.js program that takes input and creates a readme. This README was created using it.
+  ## Table of Contents:
+  ___
+  1) [Description](#description)
+  2) [Install](#installation)
+  3) [Usage](#usage)
+  4) [Video](#video)
+  5) [Collaboration](#collaboration)
+  6) [Tests](#tests)
+  7) [Questions?](#questions?)
+  8) [GitHub](#gitHub)
+  9) [License](#license)
+  ## Installation
+  ___
+  Make sure node.js is installed on local server and run node index.js
+  ## Usage
+  ___
+  to create a Readme for a project in markdown.
+  ## Video
+  ___
+  [![Demo Video](images/project.png)](images/videos/usage.mp4.webm "Demo Video")
+  ## Collaboration:
+  ___
+  If you would like to help, please follow the [Contributor Covenant](https://www.contributor-covenant.org/)
+  ## Tests
+  ___
+  N/A
+  ## Questions?
+  ___
+  Please contact me at:
+  My [GitHub](https://github.com/zaclark369)
+  
+  Or Email:
+  <zacclark369@gmail.com>
+  
+  ## License
+  ______
+  Licensed under [Creative Commons License](https://creativecommons.org/publicdomain/zero/1.0/)
+
+  
--- index.js
+++ index.js
@@ -1,93 +1,92 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generatePage = require("./src/generateMarkdown");
+const generateMarkdown = require("./src/generateMarkdown");
+// what should this const be?
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your project title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Provide a description for the project.",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Please provide installation information",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Please provide usages of your project",
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: 'How should someone collaborate with the project?',
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description for the project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide installation information",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide usages of your project",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "How should someone collaborate with the project?",
     default: `If you would like to help, please follow the [Contributor Covenant](https://www.contributor-covenant.org/)`,
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Enter the test methods for your project',
-        default: 'N/A',
-      },
-      {
-        type: 'input',
-        name: 'video',
-        message: 'Enter the path for the usage video of your project',
-      },
-      {
-        type: 'input',
-        name: 'image',
-        message: 'Enter the path for the image of your video',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your github username: ',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Enter the email address for this project: ',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Enter the license for your project: ',
-        choices: [
-          'Artistic',
-          'Creative Commons',
-          'GNU GPLv3',
-          'MIT',
-          'Mozilla',
-          'none',
-        ],
-    }
-  ]);
-};
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter the test methods for your project",
+    default: "N/A",
+  },
+  {
+    type: "input",
+    name: "video",
+    message: "Enter the path for the usage video of your project",
+  },
+  {
+    type: "input",
+    name: "image",
+    message: "Enter the path for the image of your video",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your github username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter the email address for this project: ",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Enter the license for your project: ",
+    choices: [
+      "Artistic",
+      "Creative Commons",
+      "GNU GPLv3",
+      "MIT",
+      "Mozilla",
+      "none",
+    ],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeToFile(fileName, generateMarkdown(data), (err) => 
+  return fs.writeFileSync(fileName, generateMarkdown(data), (err) =>
     console.log(err)
-    );
+  );
 }
 
-module.exports = writeToFile;
+// module.exports = writeToFile;
 
 // TODO: Create a function to initialize app
 function init() {
-    questions()
-    .then(answers)
-    .then(writeToFile('./newREADME.md', answers));
+  inquirer.prompt(questions).then(function (answers) {
+    writeToFile("README.md", {...answers});
+  });
 }
 
 // Function call to initialize app
