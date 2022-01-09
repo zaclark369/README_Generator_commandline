90958445ca5685cf3ef571bf765ffffc845ac2f7
Zachary Clark <zacclark369@gmail.com>
2022-01-09 09:15:56 -0800

    finished questions and tested, added readme generation function and init function

--- index.js
+++ index.js
@@ -1,13 +1,97 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generatePage = require("./src/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your project title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description for the project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide installation information",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide usages of your project",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: 'How should someone collaborate with the project?',
+    default: `If you would like to help, please follow the [Contributor Covenant](https://www.contributor-covenant.org/)`,
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter the test methods for your project',
+        default: 'N/A',
+      },
+      {
+        type: 'input',
+        name: 'video',
+        message: 'Enter the pathname for the usage video of your project',
+      },
+      {
+        type: 'input',
+        name: 'image',
+        message: 'Enter the pathname for the image of your video',
+      },
+      {
+        type: 'input',
+        name: 'video',
+        message: 'Enter the pathname for the usage video of your project',
+      },
+      {
+        type: 'input',
+        name: 'image',
+        message: 'Enter the pathname for the image of your video',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Enter the license for your project: ',
+        choices: [
+          'GNU AGPLv3',
+          'GNU GPLv3',
+          'GNU LGPLv3',
+          'Mozilla Public License 2.0',
+          'Apache License 2.0',
+          'MIT License',
+          'Boost Software License 1.0',
+          'The Unlicense',
+          'none',
+        ],
+    }
+  ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, generateMarkdown(data), (err) => 
+    console.log(err)
+    );
+}
+
+module.exports = writeToFile;
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+    .then(answers)
+    .then(writeToFile('./newREADME.md', answers));
+}
 
 // Function call to initialize app
 init();
